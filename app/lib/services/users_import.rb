5f83d370f2a2d5591f2ffe3ded5afd4f4f5fe4cd
iTorvolF <InvierPWNZ@gmail.com>
2022-07-07 19:50:07 +0300

    Task4/Task5

--- app/lib/services/users_import.rb
+++ app/lib/services/users_import.rb
@@ -10,22 +10,45 @@ module Services
 
       roles = Role.all.map { |role| [role.code, role.id] }.to_h
 
-      # pad_cells = true - учитывать пустые ячейки
       xlsx.each_row_streaming(pad_cells: true).each do |row|
-        id, name, email, code = parse_params(row)
-        User.find(id).update(name: name, email: email, role_id: roles[code])
+        id, name, email, code, delete = parse_params(row)
+        if id.zero? 
+          create(name, email, roles[code])
+        end
+        delete if delete == 'delete'
+        update(name, email, roles[code])
       end
     end
 
     private
 
+    def create(name, email, role)
+      new_user =
+        {
+          name: name,
+          email: email,
+          password: 123456,
+          role_id: role,
+        }
+      User.create! new_user
+    end
+
+    def delete
+      User.find(id).destroy
+    end
+
+    def update(name, email, role)
+      User.find_by(email: email).update(name: name, email: email, role_id: role)
+    end
+
     def parse_params(row)
-      [
-        row[0].value.to_i, # id
-        row[1].value,      # name
-        row[2].value,      # email
-        # row[3].value       # code
-      ]
+      array = []
+      row[0].nil? ?  array.push(0)  : array.push(row[0].value.to_i)
+      row[1].nil? ?  array.push('') : array.push(row[1].value)
+      row[2].nil? ?  array.push('') : array.push(row[2].value)
+      row[3].nil? ?  array.push('') : array.push(row[3].value)
+      row[4].nil? ?  array.push('') : array.push(row[4].value)
+      array
     end
   end
 end
