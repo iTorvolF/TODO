3a4949945e84abab4a0a542aab4c21bcf741a565
iTorvolF <InvierPWNZ@gmail.com>
2022-07-21 20:53:33 +0300

    Fix parent_id

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -18,8 +18,8 @@ class EventsController < ApplicationController
   # Просмотр конкретного задания
   def show
     authorize @event
-    # @comments = @event.comments.root.self_and_descendants.order(:lft)
-    @comments = sort_comments(@event.comments.order(:created_at))
+    @comments = @event.comments.root.self_and_descendants.order(:lft)
+    # @comments = sort_comments(@event.comments.order(:created_at))
   end
 
   # Формирует и отображает HTML-форму, которая позволяет подготовить данные для создания объекта.
