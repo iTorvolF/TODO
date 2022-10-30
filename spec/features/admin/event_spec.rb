5081c0731e79d11088af67083cb6760d6d8e536a
iTorvolF <InvierPWNZ@gmail.com>
2022-10-30 14:46:58 +0300

    admin show_event_spec

--- spec/features/admin/event_spec.rb
+++ spec/features/admin/event_spec.rb
@@ -1,6 +1,6 @@
 require 'rails_helper'
 
-RSpec.describe Admin::UsersController, driver: :selenium_chrome, js: true do
+RSpec.describe Admin::EventsController, driver: :selenium_chrome, js: true do
   let(:user) { create :admin }
   
   before do
@@ -15,6 +15,23 @@ RSpec.describe Admin::UsersController, driver: :selenium_chrome, js: true do
       sleep 1
       visit admin_events_path
       expect(page).to have_content('События')
+    end
+
+    it 'успешно отражает новое событие' do
+      @event = create(:event, user: create(:user))
+      visit admin_events_path
+      expect(page).to have_content(@event.user.name)
+    end
+  end
+
+  context :show do
+    let(:default_event) { create :event, name: 'test.event' }
+
+    it 'успешный переход' do
+      sleep 1
+      visit admin_event_path(default_event)
+      expect(page).to have_current_path admin_event_path(default_event), ignore_query: true
+      expect(page).to have_content('test.event')
     end
   end  
 end  
