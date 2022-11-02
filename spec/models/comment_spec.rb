f162fa026420dbf20a7c97ec6629ed0225d0d729
iTorvolF <InvierPWNZ@gmail.com>
2022-11-02 22:12:24 +0300

    fix comment_spec

--- spec/models/comment_spec.rb
+++ spec/models/comment_spec.rb
@@ -30,7 +30,7 @@
 require 'rails_helper'
 
 RSpec.describe Comment, type: :model do
-  subject { build(:comment) }
+  subject { create :comment }
 
   it { is_expected.to belong_to(:user) }
   it { is_expected.to belong_to(:commentable) }
